02fa7a6c5df9a3f35c2ea326c043db3fd0d1d5f9
Kavya Parekh <parekhkavya02@gmail.com>
2026-08-07 20:22:17 -0700

    Add: pacific-atlantic-water-flow - submission-0

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        R, C = len(heights), len(heights[0])
+        qP = deque()
+        qA = deque()
+        visP = set()
+        visA = set()
+
+        def bfs(q, vis):
+            while q:
+                r, c = q.popleft()
+                for dr, dc in [(0, 1), (0, -1), (1, 0), (-1,0)]:
+                    nr, nc = r+dr, c+dc
+                    if 0<=nr<R and 0<=nc<C and heights[nr][nc]>=heights[r][c] and (nr,nc) not in vis:
+                        q.append((nr, nc))
+                        vis.add((nr,nc))
+            
+            return vis
+
+        for r in range(R):
+            for c in range(C):
+                if r == 0 or c == 0:
+                    qP.append((r,c))
+                    visP.add((r,c))
+                if r == R-1 or c == C-1:
+                    qA.append((r,c))
+                    visA.add((r,c))
+        
+        visA = bfs(qA, visA)
+        visB = bfs(qP, visP)
+        return list(visA & visP)
